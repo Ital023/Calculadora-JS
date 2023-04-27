7f706666cb4bbb8ea35cb69cbe43c497d1b4ad79
Ítalo Miranda Fontenele <113559117+Ital023@users.noreply.github.com>
2023-04-27 15:33:33 -0300

    Adicionando a iluminacao function

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var botoes = document.querySelectorAll(".botao");
 var display = document.querySelector(".display");
-
+var botaoVisual = document.querySelector(".botao__muda-visual");
+var iluminacao = document.querySelectorAll(".iluminados");
+var principal = document.querySelector(".principal");
+var modo = "azul";
 botoes.forEach(botao=>{
     botao.addEventListener("click",(evento)=>{
         var valor = evento.target.textContent;
@@ -15,4 +18,30 @@ botoes.forEach(botao=>{
         }
     })
     document.onkeydown = teclado;
+})
+
+botaoVisual.addEventListener("click",()=>{
+    if(modo == "azul"){
+        iluminacao.forEach(botao=>{
+            botao.classList.add("iluminados__red");
+        })
+        principal.classList.add("principal__red");
+        modo = "vermelho";
+    }else if(modo == "vermelho"){
+        iluminacao.forEach(botao=>{
+            botao.classList.add("iluminados__green");
+        })
+        principal.classList.add("principal__green");
+        modo = "verde";
+    }else if("verde"){
+        iluminacao.forEach(botao=>{
+            botao.classList.remove("iluminados__red");
+            botao.classList.remove("iluminados__green");
+        })
+        principal.classList.remove("principal__red");
+        principal.classList.remove("principal__green");
+        modo = "azul";
+    }
+
+
 })
